756df9f664d7356eebbc245fa122dbcda8fd205c
ShreyasiPathak <shreyasi12dgp13@gmail.com>
2024-10-07 18:04:20 +0200

    changed the patch colours to yellow in visualization; added the roi vs diagnosis confusion matrix lines

--- src/analysis/visualize_roi.py
+++ src/analysis/visualize_roi.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 import cv2
+import pickle
 import matplotlib.cm as cm
 import torchvision
 
@@ -65,8 +66,10 @@ def visualize_example(config_params, input_img_case, saliency_maps_case, true_se
             cm.OrRd.set_under('w', alpha=0)
             subfigure.imshow(malignant_seg, alpha=0.85, cmap=cm.OrRd, clim=[0.9, 1])
         try:
-            #subfigure.set_title("input image: $a_{"+views_names[view_id]+"}"+f"= ${img_attention[0]:.2f}")
-            subfigure.set_title("input image: "+views_names[view_id])
+            # uncomment the below line for visualization of attention pooling based models
+            subfigure.set_title(views_names[view_id][0]+'-'+views_names[view_id][1:]+ ", $a_{\mathrm{"+views_names[view_id][0]+'-'+views_names[view_id][1:]+"}}"+f"= ${img_attention[0]:.2f}")
+            #uncomment the below line for visualizetion of mean pooling models
+            #subfigure.set_title(views_names[view_id][0]+'-'+views_names[view_id][1:])
             subfigure.axis('off')
         except:
             pass
@@ -76,18 +79,20 @@ def visualize_example(config_params, input_img_case, saliency_maps_case, true_se
         subfigure.imshow(input_img[0, 0, :, :], aspect='equal', cmap='gray')
         resized_cam_malignant = cv2.resize(saliency_maps[0,0,:,:], (W, H))
         subfigure.imshow(resized_cam_malignant, cmap=alpha_red, clim=[0.0, 1.0])
-        subfigure.set_title("SM: malignant")
+        subfigure.set_title("Saliency Map")
         subfigure.axis('off')
         
         # patch map
         subfigure = figure.add_subplot(len(views_names), total_num_subplots, view_id*9 + 3)
         subfigure.imshow(input_img[0, 0, :, :], aspect='equal', cmap='gray')
-        cm.YlGnBu.set_under('w', alpha=0)
+        #cm.YlGnBu.set_under('w', alpha=0)
+        cm.spring.set_under('w', alpha=0)
         crop_mask = gmic_utils.get_crop_mask(
             patch_locations[0, np.arange(parameters["K"]), :],
             parameters["crop_shape"], (H, W),
             "upper_left")
-        subfigure.imshow(crop_mask, alpha=0.7, cmap=cm.YlGnBu, clim=[0.9, 1])
+        #subfigure.imshow(crop_mask, alpha=0.7, cmap=cm.YlGnBu, clim=[0.9, 1])
+        subfigure.imshow(crop_mask, alpha=0.7, cmap=cm.spring, clim=[0.9, 1])
         if benign_seg is not None:
             cm.Greens.set_under('w', alpha=0)
             subfigure.imshow(benign_seg, alpha=0.85, cmap=cm.Greens, clim=[0.9, 1])
@@ -110,7 +115,7 @@ def visualize_example(config_params, input_img_case, saliency_maps_case, true_se
     plt.savefig(save_dir, bbox_inches='tight', format="pdf", dpi=500)
     plt.close()
 
-def seg_evaluation(config_params, input_img_case, img_path_list, patch_locations_case, patch_img_case, patch_attentions_case, img_attention_case, views_names, exam_name, df_score, seg_eval_metric):    
+def seg_evaluation(config_params, input_img_case, img_path_list, patch_locations_case, patch_img_case, patch_attentions_case, img_attention_case, views_names, exam_name, df_score, seg_eval_metric, pred_label, true_label, roi_diagnosis_conf_mat_all):    
     iou_any_roi_highestattnwt = []
     iou_any_roi_max = []
     iou_all_roi_mean = []
@@ -151,6 +156,8 @@ def seg_evaluation(config_params, input_img_case, img_path_list, patch_locations
         print(not_allow)
 
     if not not_allow:
+        roi_diagnosis_conf_mat_percase = np.zeros((2,4))
+        flag = 0
         for view_id, view_name in enumerate(views_names):
             input_img = input_img_case[view_id]
             input_img = torchvision.transforms.Resize((config_params['resize'][0], config_params['resize'][1]))(input_img)
@@ -247,7 +254,8 @@ def seg_evaluation(config_params, input_img_case, img_path_list, patch_locations
             if config_params['dataset'] == 'cbis-ddsm':
                 iou_view_each_roi, _, iou_highestattnwt_each_roi, fig, ax = mask_roi_match.match_to_mask_images_cbis(config_params, input_img, image_name, patch_attentions, patch_locations, seg_eval_metric, view_id, views_names, fig, ax)
             elif config_params['dataset'] == 'vindr':
-                iou_view_each_roi, _, iou_highestattnwt_each_roi, fig, ax = mask_roi_match.match_to_mask_images_vindr(config_params, input_img, image_name, patch_attentions, patch_locations, seg_eval_metric, view_id, views_names, fig, ax)
+                #iou_view_each_roi, _, iou_highestattnwt_each_roi, fig, ax = mask_roi_match.match_to_mask_images_vindr(config_params, input_img, image_name, patch_attentions, patch_locations, seg_eval_metric, view_id, views_names, fig, ax)
+                iou_view_each_roi, _, iou_highestattnwt_each_roi, fig, ax, roi_diagnosis_conf_mat_percase = mask_roi_match.roi_diagnosis_images_vindr(config_params, input_img, image_name, patch_attentions, patch_locations, seg_eval_metric, view_id, views_names, fig, ax, pred_label, true_label, roi_diagnosis_conf_mat_percase)
             elif config_params['dataset'] == 'zgt':
                 iou_view_each_roi, _, iou_highestattnwt_each_roi, fig, ax = mask_roi_match.match_to_mask_images_zgt(config_params, input_img, image_name, patch_attentions, patch_locations, seg_eval_metric, view_id, views_names, fig, ax, img_attention)
             if iou_view_each_roi!=[]:
@@ -256,6 +264,23 @@ def seg_evaluation(config_params, input_img_case, img_path_list, patch_locations
                 #iou_any_roi_highestattnwt.append(max(iou_highestattnwt_each_roi)) #V, R
                 print("max score:", max(iou_view_each_roi))
                 df_score[image_name]=[accnum, case_name, series_name, img_name, iou_view_each_roi, max(iou_view_each_roi), np.mean(iou_view_each_roi), max(iou_highestattnwt_each_roi)]
+        
+        for u in range(roi_diagnosis_conf_mat_percase.shape[0]):
+            for v in range(roi_diagnosis_conf_mat_percase.shape[1]):
+                if roi_diagnosis_conf_mat_percase[u,v] >= 1:
+                    roi_diagnosis_conf_mat_all[u,v]+=1
+                    flag = 1
+        if flag == 0:
+            if true_label == 0 and pred_label==0:
+                roi_diagnosis_conf_mat_all[0,2]+=1  
+            elif true_label == 0 and pred_label==1:
+                roi_diagnosis_conf_mat_all[0,3]+=1   
+            elif true_label == 1 and pred_label==0:
+                roi_diagnosis_conf_mat_all[1,2]+=1  
+            elif true_label == 1 and pred_label==1:
+                roi_diagnosis_conf_mat_all[1,3]+=1  
+
+                
     #bounding box 
     '''if iou_any_roi_max!=[]:
         for ax1 in ax:
@@ -269,7 +294,7 @@ def seg_evaluation(config_params, input_img_case, img_path_list, patch_locations
         iou_all_roi_mean = np.mean(np.array(iou_all_roi_mean)) #axis = 0 is across rows; average IOU over each ROI
         iou_any_roi_highestattnwt = max(iou_any_roi_highestattnwt) #max IOU over all ROIs
     return iou_any_roi_max, iou_all_roi_mean, iou_any_roi_highestattnwt'''
-    return df_score
+    return df_score, roi_diagnosis_conf_mat_all
 
 
 def modelpatch_roi_match(config_params, model, dataloader_test, df_test):
@@ -283,6 +308,10 @@ def modelpatch_roi_match(config_params, model, dataloader_test, df_test):
     dsc_sum_all_roi = 0
     dsc_sum_any_roi_hattnwt = 0
     df_dsc = {}
+    dic_pred = {}
+    roi_diagnosis_conf_mat_iou_all = np.zeros((2,4))
+    roi_diagnosis_conf_mat_dsc_all = np.zeros((2,4))
+
     #if config_params['dataset'] == 'zgt':
     #    zgt_roi_annotation = '/homes/spathak/multiview_mammogram/input_data/extracted_annotations_withrle_corrected.csv'
     #    df_roi_zgt = pd.read_csv(zgt_roi_annotation, sep=';')
@@ -358,6 +387,14 @@ def modelpatch_roi_match(config_params, model, dataloader_test, df_test):
             
             #print(img_path_list)
             test_pred = torch.ge(torch.sigmoid(output_batch_fusion), torch.tensor(0.5)).float()
+            print(test_labels)
+            print(test_pred)
+            #dic_pred[exam_name] = [test_labels.item(), test_pred.item()]
+            
+            #if test_labels.item() == 0 and test_pred.item() == 1:
+            #    print("Accession Num:", df_test.loc[test_idx.item(), 'AccessionNum'], flush=True)
+            #    input('halt')
+
             saliency_maps = saliency_map.cpu().numpy()
             try:
                 img_attns = imagelabel_attwt_match.extract_img_attn_wts(config_params, img_attns)
@@ -369,8 +406,9 @@ def modelpatch_roi_match(config_params, model, dataloader_test, df_test):
             #for calculating the intersection over union and dice similarity score of ROI candidates with the groundtruth ROI.
             #iou_any_roi_max, iou_all_roi_mean, iou_any_roi_max_highestattnwt = seg_evaluation(config_params, loaded_image, img_path_list, patch_locations, patch_imgs, patch_attentions, img_attns, views_names, exam_name, 'IOU')
             #dsc_any_roi_max, dsc_all_roi_mean, dsc_any_roi_max_highestattnwt = seg_evaluation(config_params, loaded_image, img_path_list, patch_locations, patch_imgs, patch_attentions, img_attns, views_names, exam_name, 'DSC')
-            df_iou = seg_evaluation(config_params, loaded_image, img_path_list, patch_locations, patch_imgs, patch_attentions, img_attns, views_names, exam_name, df_iou, 'IOU')
-            df_dsc = seg_evaluation(config_params, loaded_image, img_path_list, patch_locations, patch_imgs, patch_attentions, img_attns, views_names, exam_name, df_dsc,'DSC')
+            df_iou, roi_diagnosis_conf_mat_iou_all = seg_evaluation(config_params, loaded_image, img_path_list, patch_locations, patch_imgs, patch_attentions, img_attns, views_names, exam_name, df_iou, 'IOU', int(test_pred.item()), int(test_labels.item()), roi_diagnosis_conf_mat_iou_all)
+            df_dsc, roi_diagnosis_conf_mat_dsc_all = seg_evaluation(config_params, loaded_image, img_path_list, patch_locations, patch_imgs, patch_attentions, img_attns, views_names, exam_name, df_dsc,'DSC', int(test_pred.item()), int(test_labels.item()), roi_diagnosis_conf_mat_dsc_all)
+            
             '''if iou_any_roi_max!=[]:
                 df_iou[exam_name] = [exam_name, iou_any_roi_max, iou_all_roi_mean, iou_any_roi_max_highestattnwt]
                 iou_sum_any_roi = iou_sum_any_roi + iou_any_roi_max
@@ -393,6 +431,14 @@ def modelpatch_roi_match(config_params, model, dataloader_test, df_test):
             print(test_labels, test_pred)
             #input('halt')
     
+    #dbfile = open(os.path.join(config_params['path_to_output'], "label_pred_"+str(config_params['randseedother']) +'_'+ str(config_params['randseeddata'])+".pkl"), 'ab')
+    #pickle.dump(dic_pred, dbfile)
+    #dbfile.close()
+    print("roi diagnosis conf mat iou:")
+    print(roi_diagnosis_conf_mat_iou_all)
+    print("roi diagnosis conf mat dsc:")
+    print(roi_diagnosis_conf_mat_dsc_all)
+    '''
     df_iou_dsc = dict()
     for key in df_iou.keys():
         df_iou_dsc[key] = df_iou[key] + df_dsc[key][4:8]
@@ -415,6 +461,7 @@ def modelpatch_roi_match(config_params, model, dataloader_test, df_test):
     df_img_iou_dsc.loc[mean_index, 'dsc_any_roi_max_highestattnwt'] = mean_6
 
     df_img_iou_dsc.to_csv(os.path.join(config_params['path_to_output'], "iou_dsc_score_test_set_"+str(config_params['randseedother']) +'_'+ str(config_params['randseeddata'])+".csv"), sep=';',na_rep='NULL',index=False)
+    '''
     '''iou_avg_any_roi = iou_sum_any_roi/df_img_iou.shape[0]
     iou_avg_all_roi = iou_sum_all_roi/df_img_iou.shape[0]
     iou_avg_any_roi_hattnwt = iou_sum_any_roi_hattnwt/df_img_iou.shape[0]
@@ -448,7 +495,7 @@ def output_visualize(config_params, model, dataloader_test, df_test):
                 not_allow = df_roi_zgt[df_roi_zgt['AccessionNum']==df_test.loc[test_idx.item(), 'AccessionNum']]['rle_val'].isnull().all()
                 if not_allow:
                     continue
-            #if df_test.loc[test_idx.item(), 'AccessionNum'] == 6003020666: #6002394481: #6003020666: #6002466686: #6003145061: #6002626200:
+            #if df_test.loc[test_idx.item(), 'AccessionNum'] == 6003332157: #6003020666: #6002394481: #6003020666: #6002466686: #6003145061: #6002626200:
             #    print("I am in visualize", flush=True)
             if config_params['femodel'] == 'gmic_resnet18':
                 if config_params['learningtype'] == 'SIL':
